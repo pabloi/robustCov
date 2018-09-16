455c9482839941e7b3db42128b37c16b72ecb9b8
Unknown <itu.pablo@gmail.com>
2018-09-16 16:56:51 -0400

    Changed 4th argin to cholesky decomp of inverse cov

--- z2score.m
+++ z2score.m
@@ -1,4 +1,4 @@
-function z=z2score(y,Q,m,iQ)
+function z=z2score(y,Q,m,icQ)
 %Squared z-scoring for multivariate normal distributed data. Equivalent to
 %Mahalanobis distance. If y~N(m,Q) (iid), this is distributed as 
 %t^2 ~ Hotelling's T^2 = nD*(M-1)/(M-nD) F_{nD,M-nD}
@@ -18,9 +18,9 @@ end
 if nargin<4 || isempty(iQ)
     ch_Q=cholcov(Q);
     w=ch_Q'\(y-m);
-else %inverse of Q is given directly, no need to invert Q
-    ch_iQ=cholcov(iQ);
-    w=ch_iQ*(y-m);
+else %Cholesky decomp of inverse of Q is given directly, no need to invert Q
+    %icQ=cholcov(iQ);
+    w=icQ*(y-m);
 end
     z=sum(w.*w,1); %if Q,y~N(m,Q), this is distributed as t^2 ~ Hotelling's T^2 = nD*(M-1)/(M-nD) F_{nD,M-nD}, see https://en.wikipedia.org/wiki/Hotelling%27s_T-squared_distribution
     %z=sum(y.*(Q\y),1);
